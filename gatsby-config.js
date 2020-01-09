7d19f904e97d2189448b27ffce8878c69ed8eef6
Bruno Antunes <bmvantunes@gmail.com>
2020-01-09 09:16:09 +0000

    add google analytics trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-64480323-2`,
       },
     },
     `gatsby-plugin-feed`,
